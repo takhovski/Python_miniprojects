6503c928f1636681c01d8a1cfb4c0e0f9fd5a9d2
takhovski <sh.seddigh@gmail.com>
2015-11-26 23:55:54 -0700

    madlibs generator added

---
+++ madlib_gen.py
@@ -0,0 +1,8 @@
+# 3 Mad Libs Generator
+
+one = input("Enter an Adjective:  ")
+two = input("Enter an Adjective:  ")
+three = input("Enter an Adjective:  ")
+four = input("Enter a Verb:  ")
+message = " We are so %s for you! You two are %s together, and we are %s to be able to %s with you today. " %(one,two,three,four)
+print(message)
