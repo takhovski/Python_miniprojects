1693d4d697200a196029d14f6d4db1fb533efb7a
takhovski <sh.seddigh@gmail.com>
2015-11-26 23:15:17 -0700

    final version with comments

--- guess_number.py
+++ guess_number.py
@@ -1,7 +1,8 @@
 # 2. Guess the Number
+# Computer guesses a number and the user tries to guess it.
 import random
-
-def compare(x,y):
+# Function that compares two numbers and guides the user towards the right answer.
+def compare(x,y): 
     if x > y:
         print ("My number is smaller.")
         cond = True
@@ -17,10 +18,10 @@ def compare(x,y):
                 
 
 print("I am going to guess a number between 1 and 100")
-number = random.randint(1,100)
+number = random.randint(1,100) # the number that computer imagines.
 print("I did!")
 condition = True
 while condition == True:
-    temp = int(input("Guess my number. Good Luck!"))
+    temp = int(input("Guess my number. Good Luck!     "))
     condition = compare(temp,number)
     
